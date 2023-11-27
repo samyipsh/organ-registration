a48b410fc0a3ebd57c042df458f8cf9b59263cd8
samyipsh <samyipsh@gmail.com>
2023-11-27 23:26:14 +0800

    add pedal stops

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { swellInfo } from './data/stops'
-
-const numOfSwellStops = ref(swellInfo.length)
+import { pedalInfo, swellInfo } from './data/stops'
 
 const urlParams = new URLSearchParams(window.location.search)
 console.log('urlParams', urlParams)
 for (const [key, value] of urlParams) {
   console.log(key, value)
 }
+
+const numOfPedalStops = ref(pedalInfo.length)
+const pedalURLState = urlParams.get('pedal')
+if (pedalURLState == null) {
+  throw new Error('pedal URL param is null')
+}
+if (pedalURLState.length !== numOfPedalStops.value) {
+  throw new Error('pedal URL param is not the correct length')
+}
+const pedalStopsSelected = ref(pedalURLState.split('').map((x) => x === '1'))
+const togglePedalSelected = (index: number) => {
+  pedalStopsSelected.value[index] = !pedalStopsSelected.value[index]
+  const newURLState = pedalStopsSelected.value.map((x) => (x ? '1' : '0')).join('')
+  const newURL = new URL(window.location.href)
+  newURL.searchParams.set('pedal', newURLState)
+  window.history.replaceState({}, '', newURL)
+}
+
+const numOfSwellStops = ref(swellInfo.length)
 const swellURLState = urlParams.get('swell')
 if (swellURLState == null) {
   throw new Error('swell URL param is null')
@@ -27,20 +44,40 @@ const toggleSwellSelected = (index: number) => {
 </script>
 
 <template>
-  <div class="division">
-    <h2>swell ({{ numOfSwellStops }})</h2>
-    <div class="card-row">
-      <div
-        class="card"
-        v-for="(stop, index) in swellInfo"
-        :key="stop.id"
-        :class="{ 'card-selected': swellStopsSelected[index] }"
-        @click="() => toggleSwellSelected(index)"
-      >
-        <div class="card-body">
-          <h5 class="card-title">{{ stop.stopName }}</h5>
-          <p class="card-text" v-if="stop.footagePitch">{{ stop.footagePitch }}</p>
-          <p class="card-text" v-if="stop.soloVoice">{{ stop.soloVoice }}</p>
+  <div>
+    <div class="division">
+      <h2>pedal ({{ numOfPedalStops }})</h2>
+      <div class="card-row">
+        <div
+          class="card"
+          v-for="(stop, index) in pedalInfo"
+          :key="stop.id"
+          :class="{ 'card-selected': pedalStopsSelected[index] }"
+          @click="() => togglePedalSelected(index)"
+        >
+          <div class="card-body">
+            <p class="card-top" v-if="stop.soloVoice">{{ stop.soloVoice }}</p>
+            <h5 class="card-title">{{ stop.stopName }}</h5>
+            <p class="card-footer">{{ stop?.footagePitch }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="division">
+      <h2>swell ({{ numOfSwellStops }})</h2>
+      <div class="card-row">
+        <div
+          class="card"
+          v-for="(stop, index) in swellInfo"
+          :key="stop.id"
+          :class="{ 'card-selected': swellStopsSelected[index] }"
+          @click="() => toggleSwellSelected(index)"
+        >
+          <div class="card-body">
+            <p class="card-top" v-if="stop.soloVoice">{{ stop.soloVoice }}</p>
+            <h5 class="card-title">{{ stop.stopName }}</h5>
+            <p class="card-footer">{{ stop?.footagePitch }}</p>
+          </div>
         </div>
       </div>
     </div>
@@ -49,7 +86,6 @@ const toggleSwellSelected = (index: number) => {
 
 <style scoped>
 .division {
-  height: 16rem;
   width: 100vw;
   background-color: rgba(209, 209, 209, 0.711);
   padding: 2rem 0;
@@ -64,16 +100,40 @@ const toggleSwellSelected = (index: number) => {
   padding: 1rem 0;
 }
 .card {
+  width: 6rem;
   flex-shrink: 0;
   flex-grow: 0;
-  flex-direction: column;
-  justify-content: center;
   border: 1px solid #000;
   background-color: cornsilk;
-  color: #000;
-  padding: 10px;
-  width: 100px;
+  color: #111;
+  padding: 1rem 0;
   margin-right: 0.125rem;
+  border-radius: 0 0 25% 25%;
+}
+
+.card-body {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  text-align: center;
+  height: 100%;
+  min-height: 5rem;
+}
+
+.card-title {
+  font-weight: 700;
+  padding: 0 0.5rem;
+  width: 100%;
+}
+
+.card-top {
+  justify-self: flex-start;
+  flex-grow: 1;
+  height: 1.5rem;
+}
+
+.card-footer {
+  height: 1.5rem;
 }
 
 .card-selected {
--- src/data/stops.ts
+++ src/data/stops.ts
@@ -1,26 +1,44 @@
 export type Division = 'great' | 'swell' | 'pedal'
 type stopInfo = {
   stopName: string
+  id: number
   footagePitch?: string
   soloVoice?: string
-  id: number
 }
 export const swellInfo: stopInfo[] = [
-  { stopName: 'flute', footagePitch: "8'", soloVoice: 'clarinet', id: 1 },
-  { stopName: 'flute', footagePitch: "8'", soloVoice: 'cor anglais', id: 2 },
-  { stopName: 'flute', footagePitch: "8'", soloVoice: 'orch. fl', id: 3 },
-  { stopName: 'flute', footagePitch: "8'", id: 4 },
-  { stopName: 'flute', footagePitch: "8'", id: 5 },
-  { stopName: 'flute', footagePitch: "8'", id: 6 },
-  { stopName: 'flute', footagePitch: "8'", id: 7 },
-  { stopName: 'flute', footagePitch: "8'", id: 8 },
-  { stopName: 'flute', footagePitch: "8'", id: 9 },
-  { stopName: 'oboe', footagePitch: "4'", id: 10 },
-  { stopName: 'oboe', footagePitch: "4'", id: 11 },
-  { stopName: 'oboe', footagePitch: "4'", id: 12 },
-  { stopName: 'oboe', footagePitch: "4'", id: 13 },
-  { stopName: 'oboe', footagePitch: "4'", id: 14 },
-  { stopName: 'oboe', footagePitch: "4'", id: 15 },
-  { stopName: 'oboe', footagePitch: "4'", id: 16 },
-  { stopName: 'oboe', footagePitch: "4'", id: 17 }
+  { stopName: 'LIEBLICH GEDACKT', footagePitch: "8'", soloVoice: 'clarinet', id: 1 },
+  { stopName: 'GEDACKT', footagePitch: "8'", soloVoice: 'cor anglais', id: 2 },
+  { stopName: 'VIOLA POMPOSA', footagePitch: "8'", soloVoice: 'orch. fl', id: 3 },
+  { stopName: 'VIOLA CELESTE', footagePitch: "8'", id: 4 },
+  { stopName: 'OCTAVE GEIGEN', footagePitch: "8'", id: 5 },
+  { stopName: 'TRAVERSE FLUTE', footagePitch: "8'", id: 6 },
+  { stopName: 'NASARD', footagePitch: "8'", id: 7 },
+  { stopName: 'PICCOLO', footagePitch: "8'", id: 8 },
+  { stopName: 'TIERCE', footagePitch: "8'", id: 9 },
+  { stopName: 'FOURNITURE', footagePitch: "4'", id: 10 },
+  { stopName: 'WALDHORN', footagePitch: "4'", id: 11 },
+  { stopName: 'FRENCH TRUMPET', footagePitch: "4'", id: 12 },
+  { stopName: 'OBOE', footagePitch: "4'", id: 13 },
+  { stopName: 'TREMULANT', id: 14 },
+  { stopName: 'CELESTA', id: 15 },
+  { stopName: 'HARPSICHORD', id: 16 },
+  { stopName: 'SWELL UNISON OFF', id: 17 },
+  { stopName: 'MIDI ON SWELL', id: 18 },
+  { stopName: 'SOLO ORGAN VOICES', id: 19 }
+]
+
+export const pedalInfo: stopInfo[] = [
+  { stopName: 'CONTRE VIOLONE', footagePitch: "32'", id: 1 },
+  { stopName: 'DIAPASON', footagePitch: "16'", id: 2 },
+  { stopName: 'BOURDON', footagePitch: "16'", id: 3 },
+  { stopName: 'LEIBLICH GEDACKT', footagePitch: "16'", id: 4 },
+  { stopName: 'OCTAVE', footagePitch: "8'", id: 5 },
+  { stopName: 'FLUTE', footagePitch: "8'", id: 6 },
+  { stopName: 'CHORAL BASS', footagePitch: "4'", id: 7 },
+  { stopName: 'MIXTURE', footagePitch: 'III', id: 8 },
+  { stopName: 'POSAUNE', footagePitch: "16'", id: 9 },
+  { stopName: 'TROMBA', footagePitch: "8'", id: 10 },
+  { stopName: 'GREAT TO PEDAL', id: 11 },
+  { stopName: 'SWELL TO PEDAL', id: 12 },
+  { stopName: 'MIDI ON PEDAL', id: 13 }
 ]
